1c061c2f61e0c3338a5c2e75e0d55b321f727642
naikshubham <naikshubham55@gmail.com>
2018-08-11 23:32:45 +0530

    setup file

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='inc_metrics',version='1.0',
+		description="Sends an Automated mail for Incident Metrics",
+		author = "Shubham & Nikhil",
+		author_email='naikshubham55@gmail.com',
+		py_modules=['INC_METRICS']
+	)
